9d096cacd079d75f55c4386f18108677d8da5265
Fabio <fabiochirinos7@gmail.com>
2023-03-14 18:04:58 -0500

    Deployment

---
+++ README.md
@@ -0,0 +1,11 @@
+# 📌 App Noticias
+¡Hola! Esta es una aplicación desarrollada con React, una app que muestra las noticias más relevantes de USA segmentadas por categorías. Realiza una consulta a la API de [newsApi](https://newsapi.org/) y lo muestra en Cards gracias a al framework de React, MUI que hizo muy fácil el añadirle paginación. 
+
+
+## 🚀 Ejecutar la Aplicación
+Para iniciar la aplicación, debes ejecutar el comando `npm run dev` en el terminal. Si trabajas con Visual Studio Code, puedes abrir el terminal con el atajo de teclado `ctrl + ñ` si tu teclado está en español y con ``ctrl + ` `` si tu teclado está en inglés.
+
+La aplicación se abrirá automáticamente en el navegador configurado por defecto en tu dispositivo y se ejecutará en `http://localhost:5173`.
+
+## Instalar Módulos
+Para instalar los módulos necesarios para la aplicación, debes ejecutar el comando `npm install` en el terminal.
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,40 @@
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material'
+import useNoticias from '../hooks/useNoticias'
+
+
+const CATEGORIAS = [
+  { value: 'general', label: 'General' },
+  { value: 'business', label: 'Negocios' },
+  { value: 'entertainment', label: 'Entretenimiento' },
+  { value: 'health', label: 'Salud' },
+  { value: 'science', label: 'Ciencia' },
+  { value: 'sports', label: 'Deportes' },
+  { value: 'technology', label: 'Tecnología' },
+]
+
+export default function Formulario() {
+
+  const { categoria, handleChangeCategoria } = useNoticias()
+
+  return (
+    <form>
+      <FormControl fullWidth>
+        <InputLabel>Categoria</InputLabel>
+        <Select
+          label='Categoria'
+          onChange={handleChangeCategoria}
+          value={categoria}
+        >
+          {CATEGORIAS.map(categoria => (
+            <MenuItem
+              key={categoria.value}
+              value={categoria.value}
+            >
+              {categoria.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </form>
+  )
+}
